5a0202cd31ae04eb504bf834bcc444dafa168a93
Gabriel Pickl <gabriel@syntax-austria.org>
2012-04-28 16:51:37 +0200

    Implementation of ra_auth

--- src/restauth.c
+++ src/restauth.c
@@ -6,6 +6,7 @@
  */
 
 #define USER_PATH "/users/"
+#define PASSWORD_PREFIX "password="
 
 #include <curl/curl.h>
 #include <stdlib.h>
@@ -40,11 +41,20 @@ void ra_cleanup(RA_CON * con) {
 
 int ra_auth(const RA_CON * con, const char * user, const char * password) {
 	CURL * session = ra_init_curl(con);
-	char * postdata = malloc(strlen(con->address)+strlen(USER_PATH)+strlen(user)+1);
-	sprintf(postdata, "%s%s%s", con->address, USER_PATH, user);
-	curl_easy_setopt(session, CURLOPT_POSTFIELDS, postdata);
-	free(postdata);
+	char * address = malloc(strlen(con->address)+strlen(USER_PATH)+strlen(user)+1);
+	sprintf(address, "%s%s%s", con->address, USER_PATH, user);
+	curl_easy_setopt(session, CURLOPT_URL, address);
+	free(address);
+
+	char * post_data = malloc(strlen(PASSWORD_PREFIX)+strlen(password)+1);
+	sprintf(post_data, "%s%s", PASSWORD_PREFIX, password);
+	char * esc_post_data = ra_str_escape(post_data);
+	free(post_data);
+	curl_easy_setopt(session, CURLOPT_POSTFIELDS, esc_post_data);
+	free(esc_post_data);
+
 	CURLcode err = curl_easy_perform(session);
+
 	if(err != 0) {
 		ar_curl_error(err);
 		return -1;
